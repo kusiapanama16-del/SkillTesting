9ef629fcc588b311d30a3740a819e59957f9ad0b
synteravin <kusiapanama16@gmail.com>
2026-03-29 21:23:31 +0700

    update

--- app/Services/Character/UploadCharacterAvatar.php
+++ app/Services/Character/UploadCharacterAvatar.php
@@ -5,20 +5,19 @@ namespace App\Services\Character;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Filesystem\FilesystemAdapter;
 
 class UploadCharacterAvatar
 {
-    public function upload($file)
+    public function upload(UploadedFile $file): string
     {
-        
         $filename = 'char_' . Str::uuid() . '.' . $file->getClientOriginalExtension();
 
-        $path = Storage::disk('cloud')->putFileAs(
-            'characters',
-            $file,
-            $filename
-        );
+        /** @var FilesystemAdapter $disk */
+        $disk = Storage::disk('cloud');
 
-        return Storage::disk('cloud')->url($path); // ✅ FIX
+        $path = $disk->putFileAs('characters', $file, $filename);
+
+        return $disk->url($path);
     }
 }
